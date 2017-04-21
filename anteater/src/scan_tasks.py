f3ee31ee7b0e2b94ae8c9d35348513158d7e610f
lhinds <lhinds@redhat.com>
2017-04-21 12:00:20 +0100

    Added futher template types

--- anteater/src/scan_tasks.py
+++ anteater/src/scan_tasks.py
@@ -12,7 +12,7 @@ from binaryornot.check import is_binary
 
 logger = antlog.Logger(__name__).getLogger()
 wk_dir = os.path.dirname(os.path.realpath('__file__')) + '/'
-
+licence_templates = os.path.join(wk_dir + 'licence-templates.yaml')
 
 def scan_all(reports_dir, repos_dir):
     """ As it says on the tin. Performs complete scan of allocated
@@ -34,17 +34,24 @@ def scan_project(reports_dir, project, scanner, repos_dir):
     php = False
     perl = False
     projdir = repos_dir + project
-    global lang
+    # Perform rudimentary scans
+    run_binfind(project, projdir)
+    run_secretsearch(project, projdir)
+    run_licence_check(project, projdir)
+
     if scanner:
+        print ("IS SCANNER")
         if scanner == 'bandit':
             run_bandit(reports_dir, project, projdir)
         elif scanner == 'pmd':
             run_pmd(reports_dir, project, projdir)
+            pass
         elif scanner == 'rats':
             run_rats(reports_dir, project, projdir)
         else:
             logger.error("%s is not a recognised scanner tool", scanner)
     else:
+        print("NOT SCANNER")
         """Let's try to guess which scanner to use, by file extension"""
         for root, dirs, files in os.walk(projdir):
             for file in files:
@@ -62,31 +69,18 @@ def scan_project(reports_dir, project, scanner, repos_dir):
                     perl = True
         # Project contains only python files
         if py and not (java or c):
-            lang = "pybash"
             run_bandit(reports_dir, project, projdir)
-            run_binfind(project, projdir)
-            run_secretsearch(project, projdir)
-            run_licence_check(project, projdir)
         # Project contains c files
         if c and not java:
             run_rats(reports_dir, project, projdir)
-            run_binfind(project, projdir)
-            run_secretsearch(project, projdir)
-            run_licence_check(project, projdir)
         # Project contains only java files
         if java and not (py or c):
             run_pmd(reports_dir, project, projdir)
-            run_binfind(project, projdir)
-            run_secretsearch(project, projdir)
         # Project contains a mix of c and python
         if c and py and not java:
             run_rats(reports_dir, project, projdir)
-            run_binfind(project, projdir)
-            run_secretsearch(project, projdir)
         if rb or php or perl:
             run_rats(reports_dir, project, projdir)
-            run_binfind(project, projdir)
-            run_secretsearch(project, projdir)
 
 
 def run_bandit(reports_dir, project, projdir):
@@ -143,17 +137,17 @@ def run_binfind(project, projdir):
         except KeyError, e:
             logger.info('No project waivers for project: {0}'.format(project))
 
-        for root, dirs , files in os.walk(projdir):
+        for root, dirs, files in os.walk(projdir):
             for items in files:
-               fullpath = os.path.join(root, items)
-               bincheck = is_binary(fullpath)
-               words_re = re.compile("|".join(waiverlist), flags=re.IGNORECASE)
+                fullpath = os.path.join(root, items)
+                bincheck = is_binary(fullpath)
+                words_re = re.compile("|".join(waiverlist), flags=re.IGNORECASE)
 
-               if not words_re.search(fullpath) and bincheck:
-                   logger.error('Non white listed binary found: {0}'.format(fullpath))
+                if not words_re.search(fullpath) and bincheck:
+                    logger.error('Non white listed binary found: {0}'.format(fullpath))
 
-                   with open("anteater-gate.log", "a") as gatereport:
-                       gatereport.write('Non white listed binary found: {0}\n'.format(fullpath))
+                    with open("anteater-gate.log", "a") as gatereport:
+                        gatereport.write('Non white listed binary found: {0}\n'.format(fullpath))
 
 
 def run_secretsearch(project, projdir):
@@ -169,18 +163,18 @@ def run_secretsearch(project, projdir):
         file_contents_re = re.compile("|".join(file_contents), flags=re.IGNORECASE)
 
         try:
-             waiver_files = (yl['waivers'][project]['file_names'])
-             if waiver_files:
-                 waiver_files_re = re.compile("|".join(waiver_files), flags=re.IGNORECASE)
-                 waiver_files_set = True
+            waiver_files = (yl['waivers'][project]['file_names'])
+            if waiver_files:
+                waiver_files_re = re.compile("|".join(waiver_files), flags=re.IGNORECASE)
+                waiver_files_set = True
         except:
             waiver_files_set = False
 
         try:
-             waiver_contents = (yl['waivers'][project]['file_contents'])
-             if waiver_contents:
-                 waiver_contents_re = re.compile("|".join(waiver_contents), flags=re.IGNORECASE)
-                 waiver_contents_set = True
+            waiver_contents = (yl['waivers'][project]['file_contents'])
+            if waiver_contents:
+                waiver_contents_re = re.compile("|".join(waiver_contents), flags=re.IGNORECASE)
+                waiver_contents_set = True
         except:
             waiver_contents_set = False
 
@@ -191,13 +185,13 @@ def run_secretsearch(project, projdir):
                 fullpath = os.path.join(root, items)
                 if waiver_files_set:
                     if not waiver_files_re.search(fullpath) and file_names_re.search(fullpath):
-                       logger.info('Found what looks like a blacklisted file: {0}'.format(fullpath))
-                       with open("anteater-gate.log", "a") as gatereport:
-                           gatereport.write('Found what looks like a blacklisted file: {0}\n'.format(fullpath))
+                        logger.info('Found what looks like a blacklisted file: {0}'.format(fullpath))
+                        with open("anteater-gate.log", "a") as gatereport:
+                            gatereport.write('Found what looks like a blacklisted file: {0}\n'.format(fullpath))
                 else:
                     if file_names_re.search(fullpath):
                         logger.info('Found what looks like a blacklisted file: {0}'.format(fullpath))
-                        with open("anteater-gate.log", "a") as gatereport:
+                        with open('anteater-gate.log', "a") as gatereport:
                            gatereport.write('Found what looks like a blacklisted file: {0}\n'.format(fullpath))
 
                 if not is_binary(fullpath):
@@ -220,24 +214,37 @@ def run_secretsearch(project, projdir):
 
 def run_licence_check(project, projdir):
     logger.info('Running Licence Check on: {0}'.format(project))
-    licence_templates = os.path.join(wk_dir + 'licence-templates.yaml')
-    with open(licence_templates, 'r') as f:
-           yl = yaml.safe_load(f)
-    template = (yl[lang])
     for root, dirs, files in os.walk(projdir):
         for item in files:
+            fullpath = os.path.join(root, item)
             if item.endswith(('.py', '.sh')):
-                template = (yl['pybash'])
-                fullpath = os.path.join(root, item)
-                fo = open(fullpath, 'r')
-                content = fo.read()
-                processed = re.sub(r'(?<=2017 )(.*)(?=and others)', '', content) # use a re.compile here
-                if template in processed:
-                    logger.info('Licence Check passed for: {0}'.format(fullpath))
-                else:
-                    logger.error('No License file within: {0}'.format(fullpath))
-                    with open("anteater-gate.log", "a") as gatereport:
-                        gatereport.write('No License File found within: {0}\n'.format(fullpath))
+                file_type = 'pybash'
+                license_regex(fullpath, item, file_type)
+            elif item.endswith(('.c', '.cc', '.cpp', '.h', '.java')):
+                file_type = 'cjava'
+                license_regex(fullpath, item, file_type)
+            elif item.endswith('xml'):
+                file_type = 'xml'
+                license_regex(fullpath, item, file_type)
+            elif item.endswith('patch'):
+                file_type = 'patch'
+                license_regex(fullpath, item, file_type)
+            elif item.endswith('rst'):
+                file_type = 'rst'
+                license_regex(fullpath, item, file_type)
 
 
+def license_regex(fullpath, item, file_type):
+    with open(licence_templates, 'r') as f:
+        yl = yaml.safe_load(f)
+    template = (yl[file_type])
 
+    fo = open(fullpath, 'r')
+    content = fo.read()
+    processed = re.sub(r'(?<=2017 )(.*)(?=and others)', '', content) # use a re.compile here
+    if template in processed:
+        logger.info('Licence Check passed for: {0}'.format(fullpath))
+    else:
+        logger.error('No License file within: {0}'.format(fullpath))
+        with open("anteater-gate.log", "a") as gatereport:
+            gatereport.write('No License File found within: {0}\n'.format(fullpath))
