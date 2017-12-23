626e17cab32bbbc4050aac85a25c1dcc40cd9350
lhinds <lhinds@redhat.com>
2017-12-23 18:48:24 +0000

    Release 0.15

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def read(fname):
 
 setup(
     name='anteater',
-    version="0.14",
+    version="0.15",
     description='Anteater - CI Validation Framework',
     long_description=read("README.md"),
     author='Luke Hinds',
