28b08d7f7924b2fceb102de3a48811f4f4bff32e
lhinds <lhinds@redhat.com>
2018-03-29 14:43:00 +0100

    Fixing gate failures

--- tests/test_docopt.py
+++ tests/test_docopt.py
@@ -4,12 +4,14 @@ import anteater
 
 from anteater.main import __doc__ as doc
 
+
 class TestClient(unittest.TestCase):
     def testArgs(self):
         cmd = '--ips --urls --binaries --project test --patchset /home/user/repo/patchset'
         args = docopt(doc, cmd.split(' '))
         self.assertEqual(args['<project>'], 'test')
         self.assertEqual(args['<patchset>'], '/home/user/repo/patchset')
+
 
 if __name__ == "__main__":
     unittest.main()
